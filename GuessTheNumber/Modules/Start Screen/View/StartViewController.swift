d2d854fc71733db5696b368640965525232c49d1
ArtemyKv <artemykv@gmail.com>
2023-02-13 06:33:45 +0500

    edit typo in start screen rules alert

--- GuessTheNumber/Modules/Start Screen/View/StartViewController.swift
+++ GuessTheNumber/Modules/Start Screen/View/StartViewController.swift
@@ -42,7 +42,7 @@ class StartViewController: UIViewController {
 extension StartViewController: StartViewProtocol {
     func presentAlert(title: String?, messasge: String?) {
         let alert = UIAlertController(title: title, message: messasge, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Dismess", style: .cancel)
+        let action = UIAlertAction(title: "Dismiss", style: .cancel)
         alert.addAction(action)
         self.present(alert, animated: true)
     }
